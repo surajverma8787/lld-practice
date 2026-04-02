2d0ee40f10c97305e590916900753fbba0126c95
surajverma8787 <surajverma8787@gmail.com>
2026-04-03 01:04:24 +0530

    added payment with strategy pattern

--- StrategyPattern/StrategyPatternPayment.java
+++ StrategyPattern/StrategyPatternPayment.java
@@ -1,25 +1,77 @@
-class PaymentProcessor {
-    void processPayment(String type, double amount) {
-        switch (type) {
-            case "credit_card":
-                System.out.println("Using Credit Card");
-                  break;
-            case "net_banking":
-                System.out.println("Using net banking");
-                  break;
-            case "cash":
-                System.out.println("Using cash");
-                  break;
-            default:
-                System.out.println("undefined type");
-                  break;
-        }
+// Payment Processor - Without Strategy Pattern
+
+// class PaymentProcessor {
+//     void processPayment(String type, double amount) {
+//         switch (type) {
+//             case "credit_card":
+//                 System.out.println("Using Credit Card");
+//                   break;
+//             case "net_banking":
+//                 System.out.println("Using net banking");
+//                   break;
+//             case "cash":
+//                 System.out.println("Using cash");
+//                   break;
+//             default:
+//                 System.out.println("undefined type");
+//                   break;
+//         }
+//     }
+// }
+
+// public class StrategyPatternPayment {
+//     public static void main(String[] args) {
+//         PaymentProcessor processor = new PaymentProcessor();
+//         processor.processPayment("credit_card", 20);
+//     }
+// }
+
+interface PaymentStrategy {
+    public void processPayment(double amount);
+}
+
+class CreditCardPayment implements PaymentStrategy {
+    String cardNumber;
+
+    CreditCardPayment(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Paying using credit card");
+    }
+}
+
+class NetBankingPayment implements PaymentStrategy {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Paying using net banking");
+    }
+}
+
+class CashPayment implements PaymentStrategy {
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Paying using cash");
+    }
+}
+
+class ProcessPayment {
+    PaymentStrategy paymentStrategy;
+
+    ProcessPayment(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void processPayment(double amount) {
+        paymentStrategy.processPayment(amount);
     }
 }
 
 public class StrategyPatternPayment {
     public static void main(String[] args) {
-        PaymentProcessor processor = new PaymentProcessor();
-        processor.processPayment("credit_card", 20);
+        ProcessPayment process = new ProcessPayment(new CreditCardPayment("123"));
+        process.processPayment(12);
     }
 }
